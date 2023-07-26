754c1937568bf7ebdb58765dab69406d86724c9f
Seba-Concheso <sebastianconcheso@gmail.com>
2023-07-26 09:28:16 -0300

    listo para deploy

--- client/src/components/Country/Country.jsx
+++ client/src/components/Country/Country.jsx
@@ -1,38 +1,19 @@
 import { Link } from "react-router-dom";
+import Style from "./Country.module.css";
 
 const Country = (country, Activity) => {
   return (
-    <div class="card text-center">
+    <div className={Style.country}>
+      {/* <h4 className={Style.id}>{country.id}</h4> */}
       <Link to={`/countries/${country.id}`}>
-        <img class="card-img-top" src={country.flag} alt={country.name} />
+        <img className={Style.image} src={country.flag} alt={country.name} />
       </Link>
-      <div class="card-body">
-        <h4 class="card-title">{country.name}</h4>
-        <h4 class="card-text text-secondary">Continente: {country.continent}</h4>
-        <h4>{Activity.name}</h4>
-      </div>
+      <h4 className={Style.name}>{country.name}</h4>
+
+      <h3 className={Style.continent}>Continente: {country.continent}</h3>
+      <h4>{Activity.name}</h4>
     </div>
   );
 };
 
 export default Country;
-
-// import { Link } from "react-router-dom";
-// import Style from "./Country.module.css";
-
-// const Country = (country, Activity) => {
-//   return (
-//     <div className={Style.country}>
-//       {/* <h4 className={Style.id}>{country.id}</h4> */}
-//       <Link to={`/countries/${country.id}`}>
-//         <img className={Style.image} src={country.flag} alt={country.name} />
-//       </Link>
-//       <h4 className={Style.name}>{country.name}</h4>
-
-//       <h3 className={Style.continent}>Continente: {country.continent}</h3>
-//       <h4>{Activity.name}</h4>
-//     </div>
-//   );
-// };
-
-// export default Country;
--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
